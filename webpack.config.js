c8f2fd87d620cfc87a400a8f940564c433c07cf7
yair.ansbacher <yairans@gmail.com>
2020-08-13 17:33:32 +0300

    feat(FEC-10347): expose kaltura player as a global variable instead of UMD

--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,6 @@ module.exports = {
     path: __dirname + '/dist',
     filename: '[name].js',
     library: ['KalturaPlayer', 'cast', 'sender'],
-    libraryTarget: 'umd',
     umdNamedDefine: true,
     devtoolModuleFilenameTemplate: './cast/sender/[resource-path]'
   },
@@ -61,11 +60,6 @@ module.exports = {
     modules: [path.resolve(__dirname, 'src'), 'node_modules']
   },
   externals: {
-    'kaltura-player-js': {
-      commonjs: 'kaltura-player-js',
-      commonjs2: 'kaltura-player-js',
-      amd: 'kaltura-player-js',
-      root: ['KalturaPlayer']
-    }
+    'kaltura-player-js': ['KalturaPlayer']
   }
 };
