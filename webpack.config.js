90a4cfa603766d8b1fc2442cdc29715b2744d583
Yair Ansbacher <yair.ansbacher@kaltura.com>
2020-09-08 15:27:25 +0300

    feat(FEC-10347): expose kaltura player as a global variable instead of UMD (#41)
    set `libraryTarget:'var'` as default.
    Change the externals to use the root only.
    
    BREAKING CHANGE: This package is not UMD anymore
    
    Solves FEC-10347

--- webpack.config.js
+++ webpack.config.js
@@ -20,8 +20,6 @@ module.exports = {
     path: __dirname + '/dist',
     filename: '[name].js',
     library: ['KalturaPlayer', 'cast', 'sender'],
-    libraryTarget: 'umd',
-    umdNamedDefine: true,
     devtoolModuleFilenameTemplate: './cast/sender/[resource-path]'
   },
   devtool: 'source-map',
@@ -61,11 +59,6 @@ module.exports = {
     modules: [path.resolve(__dirname, 'src'), 'node_modules']
   },
   externals: {
-    'kaltura-player-js': {
-      commonjs: 'kaltura-player-js',
-      commonjs2: 'kaltura-player-js',
-      amd: 'kaltura-player-js',
-      root: ['KalturaPlayer']
-    }
+    'kaltura-player-js': ['KalturaPlayer']
   }
 };
